804508e649a602c5179ca4bbc4415090552e34da
Sadie Apodaca <apo25002@byui.edu>
2026-07-18 19:33:41 -0600

    project

---
+++ personal site/page1.js
@@ -0,0 +1,54 @@
+const keychainImg = ["tinified/pink-mew.jpg", "tinified/ninja-turtle.jpg", "tinified/unicorn.jpg"];
+const animalImg = ["tinified/blue-dino.jpg", "tinified/teal-octopus.jpg", "tinified/yellow-duck.jpg"];
+const bagImg = ["tinified/gray-bag.jpg", "tinified/purple-pattern.jpg"];
+const blanketImg = ["tinified/blue-zig-zag.jpg", "tinified/flower-blanket.jpg", "tinified/orange-blue-blanket.jpg"];
+
+keychainImg.forEach(key => {
+        //Display a new image for each item in the array.
+
+        const img = `
+            <img src="${key}" style="width:30%"/>
+        `;
+        document.getElementById("keys").innerHTML += img;
+    
+});
+
+animalImg.forEach(ani => {
+        //Display a new image for each item in the array.
+
+        const img = `
+            <img src="${ani}" style="width:30%"/>
+        `;
+        document.getElementById("animals").innerHTML += img;
+    
+});
+
+bagImg.forEach(bag => {
+        //Display a new image for each item in the array.
+
+        const img = `
+            <img src="${bag}" style="width:30%"/>
+        `;
+        document.getElementById("bags").innerHTML += img;
+    
+});
+
+blanketImg.forEach(blk => {
+        //Display a new image for each item in the array.
+
+        const img = `
+            <img src="${blk}" style="width:30%"/>
+        `;
+        document.getElementById("blankets").innerHTML += img;
+    
+});
+
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "150px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
--- personal site/page2.js
+++ personal site/page2.js
@@ -72,3 +72,11 @@ function productChange() {
 
 
 
+function openNav() {
+  document.getElementById("mySidenav").style.width = "150px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
